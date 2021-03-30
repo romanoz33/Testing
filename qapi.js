3e1572e08fa49ccd8ecb19f1e7b927de416fb807
Roman BAD <57302328+romanoz33@users.noreply.github.com>
2021-03-30 16:48:08 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"605dc265f2b826001ebea45c",
+				"605dc265f2b826001ebea45e"
+			]
+		},
+		"605dc265f2b826001ebea45c": {
+			"id": "605dc265f2b826001ebea45c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"605dc265f2b826001ebea45e": {
+			"id": "605dc265f2b826001ebea45e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,159 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Strong, Icon } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Section, Override, StackItem, Stack } from "@quarkly/components";
+import * as Components from "components";
+import { MdRemoveRedEye } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Box width="300px" margin="20px auto 20px auto" background="#50e9ff">
+			<Text font="31px sans-serif" text-align="center">
+				<Strong>
+					Views
+				</Strong>
+			</Text>
+			<Components.Counter
+				revers={false}
+				text-align="center"
+				numberSuffix="%"
+				numberPrefix="$"
+				endingNumber="700"
+				startingNumber="230"
+			>
+				<Override
+					slot="Counter"
+					margin="0px auto 0px auto"
+					color="#ffffff"
+					display="block"
+					border-color="#000000"
+				/>
+			</Components.Counter>
+			<Icon category="md" icon={MdRemoveRedEye} size="50px" margin="20px auto 0px auto" />
+		</Box>
+		<Section padding="64px 0" sm-padding="40px 0" color="--dark">
+			<Stack>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h2" font="--headline1" margin="0 0 8px 0">
+						FAQs
+					</Text>
+					<Text as="p" font="--lead" margin="0" color="--greyD2">
+						Need answers? We got’em
+					</Text>
+				</StackItem>
+				<StackItem width="66.66%" md-width="100%">
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								Is there life on Mars?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								Why is the sky blue?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								When will we get there?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								How much salt is in the ocean?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
